ecfd0a9742cba587604be9b3d6be62883266b27b
Paramountworldrecords <admin@paramountworldrecords.com>
2026-03-29 18:57:41 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://paramountworldrecords.com',
-  integrations: [sitemap()],
+  site: 'https://www.paramountworldrecords.com',
+  base: '/',
+  integrations: [tailwind(), sitemap()],
 });
